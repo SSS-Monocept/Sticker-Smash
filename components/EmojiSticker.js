fd917406a43565ddbc53b6d34cb55e05ed6e4804
SSS-Monocept <shouryasinghsengar@monocept.com>
2024-06-24 13:17:07 +0530

    Added a pan gesture : to drag added sticker

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -7,7 +7,14 @@ import Animated, {
 import { View } from "react-native";
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const tranlateX = useSharedValue(0);
+  const tranlateY = useSharedValue(0);
   const scaleImage = useSharedValue(imageSize);
+
+  const drag = Gesture.Pan().onChange((event) => {
+    tranlateX.value += event.changeX;
+    tranlateY.value += event.changeY;
+  });
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -17,6 +24,20 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
       }
     });
 
+  const containerStyle = useAnimatedStyle(() => {
+    // hook to return an array of transforms.
+    return {
+      transform: [
+        {
+          translateX: tranlateX.value,
+        },
+        {
+          translateY: tranlateY.value,
+        },
+      ],
+    };
+  });
+
   const imageStyle = useAnimatedStyle(() => {
     return {
       width: withSpring(scaleImage.value),
@@ -25,14 +46,16 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
   });
 
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
