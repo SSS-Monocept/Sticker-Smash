4f1148b99fce0232bc2bd93b9c320a6d24de2981
SSS-Monocept <shouryasinghsengar@monocept.com>
2024-06-24 15:56:31 +0530

    Configuring Splash Screen and StatusBar

--- App.js
+++ App.js
@@ -130,7 +130,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
